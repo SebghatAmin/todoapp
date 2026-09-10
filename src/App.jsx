a08614a2aefc64279e5936fd10197076bbb2aa5e
Office Laptop <sebghatullah1994@gmail.com>
2026-09-10 14:40:37 +0430

    some changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import Todo from "./components/Todo";
-import Header from "./components/Header";
-import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Header />
-
       <Todo />
     </div>
   );
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,15 @@
+import { createLogger } from "vite";
 import styles from "./todoitem.module.css";
 export default function TodoItem({ item }) {
+    function handleDelete(){
+        console.log("delet btn clicked")
+    }
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
         {item}
         <span>
-          <button className={styles.deleteButton}>x</button>
+          <button  onClick={handleDelete} className={styles.deleteButton}>x</button>
         </span>
       </div>
       <hr className={styles.line} />
