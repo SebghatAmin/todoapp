5d1be9435c6d886209c45c2ad36e7c5690e0b0ea
Office Laptop <sebghatullah1994@gmail.com>
2026-09-12 18:16:32 +0430

    done

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,16 +1,12 @@
 import styles from "./todoitem.module.css";
 
-export default function TodoItem({ item, onDelete, onToggle }) {
-  const itemClassName = item.done ? styles.completed : "";
-
+export default function TodoItem({ item, onDelete }) {
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        <span className={itemClassName} onClick={onToggle}>
-          {item.name}
-        </span>
+        {item.name}
         <span>
-          <button onClick={onDelete} className={styles.deleteButton}>
+          <button onClick={()=>onDelete(item)} className={styles.deleteButton}>
             x
           </button>
         </span>
